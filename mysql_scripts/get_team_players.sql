247df29d1f65179e822612328bea2f46a679d6e1
cake-dev <developer.montana@gmail.com>
2023-03-08 11:53:53 -0700

    sync

--- mysql_scripts/get_team_players.sql
+++ mysql_scripts/get_team_players.sql
@@ -2,7 +2,10 @@ SELECT p.player_id,
     p.gamer_name,
     p.player_name,
     p.team_id,
-    p.player_role
+    p.player_role,
+    p.player_country,
+    p.player_region,
+    p.player_rank
 FROM PLAYER p
     INNER JOIN TEAM t ON p.team_id = t.team_id
-WHERE t.team_name = 'Team Secret';
+WHERE t.team_name = 'Thiuth Gaming';
